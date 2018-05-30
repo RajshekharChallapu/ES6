06fd240b6ebb753d4eaac3e9d5f92c08cd3c9fb6
CR <rajshekhar@protonmail.com>
2018-05-29 20:25:26 -0500

    for loop and .forEach()

--- Helpers/forEach.js
+++ Helpers/forEach.js
@@ -1,14 +1,24 @@
-//Create an array of nunbers
-var numbers = [1,2,3,4,5];
+//regular for loop you have three steps:
 
-//Create a variable to hold the sum
-var sum=0;
-
-function adder(number){
-    sum+=number;
+//1.Define an iterator value: var i = 0;
+//2.Define an end point: i < array.length;
+//3.Tell how the loop how should iterate: i++;
+//for loop 
+var array = [1, 2, 3];
+for (var i = 0; i < array.length; i++) {
+  console.log(i);
 }
-//Loop over the array, incrementing the sum variable
-numbers.forEach(adder);
 
-//print the sum variable
-sum;
+//with .forEach() you simply pass a function that is executed on each element in the array
+
+//   .forEach()   
+var array =[1, 2, 3];
+array.forEach(function(i) {
+  console.log(i);
+});
+
+//When using the .forEach() you pass an individual function with it’s own scope. In a for loop you’re polluting whatever scope you place the loop in. Most, if not all, of the time, this is a bad thing.
+
+
+//When to use
+//.forEach() is great you need to execute a function for each individual element in an array. Good practice is that you should use .forEach() when you can’t use other array methods to accomplish your goal. I know this may sound vague, but .forEach() is a generic tool… only use it when you can’t use a more specialized tool.
