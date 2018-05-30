0d56149c2fa04d374d931c3b4c15301d6b980436
CR <rajshekhar@protonmail.com>
2018-05-29 22:20:59 -0500

    filteter with function

--- Helpers/filter.js
+++ Helpers/filter.js
@@ -1,26 +1,18 @@
 
-//.filter() when you want to select a subset of multiple elements from an array.
-//The filter() method creates a new array with all elements that pass the test implemented by the provided function.
-var products = [
-  { name: "cucumber", type: "vegetable" },
-  { name: "banana", type: "fruit" },
-  { name: "celery", type: "vegetable" },
-  { name: "orange", type: "fruit" }
+
+var products=[
+
+    {name:'cucumber', type:'vegetable', quantity:0,price:1},
+    {name:'banana ', type:'fruit', quantity:10,price:15},
+    {name:'clery', type:'vegetable', quantity:30,price:9},
+    {name:'orange', type:'fruit', quantity:3,price:5}
 ];
 
-var filteredProducts = [];
-//takes out of element from source array, pass it into our iterator function …this iterator functiuon has to return either the truthy or falsey values….if it is true it will included in new result array..if it is false it won’t..
-for (var i = 0; i < products.length; i++) {
-  if (products[i].type === "fruit") {
-    filteredProducts.push(products[i]);
-  }
-}
-filteredProducts;
-products.filter(function(product){
-/* if(product.type === 'vegetable'){
-    return true;} */
+//type is 'vegetable', quantity is greater that 0, price is less than 10
 
-    return product.type === 'fruit';
-    
+products.filter(function(product){
+    return product.type=== 'vegetable'
+    && product.quantity>0 
+    && product.price<10
 
 });
