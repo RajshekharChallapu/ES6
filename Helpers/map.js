6de1e0e521f31752672fadc28234fed4280a2468
CR <rajshekhar@protonmail.com>
2018-05-29 20:34:35 -0500

    map() use

--- Helpers/map.js
+++ Helpers/map.js
@@ -1,5 +1,6 @@
 
 //map is used whenever we want to modify some list of array data.....
+//.map() when you want to transform elements in an array.
 var cars = [
     { model: 'Buick', price:'CHEAP' },
     {model:'Camaro', price:'expensive'}
@@ -11,3 +12,4 @@ return cars.price;
 
 
 prices;
+
