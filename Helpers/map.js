acae288cf4ce5813d4ad31c6e9f48a47a5c3d8db
CR <rajshekhar@protonmail.com>
2018-05-29 20:49:43 -0500

    map ()....example

--- Helpers/map.js
+++ Helpers/map.js
@@ -13,3 +13,15 @@ return cars.price;
 
 prices;
 
+//example
+//Ui=sing map,create a new array that contains the 'height' property of each object.Assign this new array to the variable 'heights'.Don't forget to use the 'return' keyword in the funciton\.
+var images=[
+    {heights:'40px', width:'40'},
+    {heights:'70px', width:'50'},
+    {heights:'80px', width:'60'}
+];
+
+var heights = images.map(function(){
+    return images.height;
+});
+heights;
